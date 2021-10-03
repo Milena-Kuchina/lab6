9398bccb3d3943b44cdd6bed8251864f48877c9e
Milena-Kuchina <91850857+Milena-Kuchina@users.noreply.github.com>
2021-10-03 23:41:48 +0300

    Add files via upload

---
+++ CityCar.java
@@ -0,0 +1,10 @@
+package lab6;
+
+public class CityCar extends Car{
+    public CityCar(String mark,double weight, Engine engine){
+        this.mark=mark;
+        this.weight=weight;
+        this.engine=engine;
+        this.kind="City Car";
+    }
+}
